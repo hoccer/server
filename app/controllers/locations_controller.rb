77c496da181166a60933d34a3d1933e130db0d6e
hukl <contact@smyck.org>
2009-07-08 11:22:12 +0200

    only returning the last thrown file for now

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,7 +4,7 @@ class LocationsController < ApplicationController
     host = "#{request.protocol}#{request.env["HTTP_HOST"]}"
     
     location = Location.find_by_coordinates(params[:id])
-    response = {:files => location.uploads.map{|x| host + x.attachment.url}}.to_json
+    response = {:files => [location.uploads.last].map{|x| host + x.attachment.url}}.to_json
     render :json => response
   end
   

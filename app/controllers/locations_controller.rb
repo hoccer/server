d91e28791091b46b82d7071b6722bca02bbaf288
hukl <contact@smyck.org>
2009-07-22 14:55:23 +0200

    temporary fix for receiving search request before the seeding gesture. needs beautification

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -14,6 +14,13 @@ class LocationsController < ApplicationController
     options     = coordinats.merge(:gesture => params[:gesture])
     
     gestures    = get_gestures(options)
+    counter     = 0
+    
+    while gestures.empty? && counter < 5
+      sleep(1)
+      gestures = get_gestures(options)
+      counter += 1
+    end
     
     response    = {
       :uploads => gestures.map {|g| "#{host}/uploads/#{g.upload.checksum}"}
@@ -24,11 +31,6 @@ class LocationsController < ApplicationController
   
   
   def get_gestures options
-    unless gestures = Location.find_gestures(options)
-      sleep(1)
-      get_gestures(options)
-    else
-      return gestures
-    end
+    gestures = Location.find_gestures(options)
   end
 end

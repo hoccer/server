867acdb0f0a1fd157ff9905a59a13ff1f43a4183
hukl <contact@smyck.org>
2009-08-04 15:23:56 +0200

    lots of refactoring and streamlining

--- public/javascripts/hoccer.js
+++ public/javascripts/hoccer.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  initialize();
+  maps.initialize();
+  uploader.initialize();
   
   /* Ajaxify the submit form */
   $("#submit").click(function(){      
@@ -9,29 +10,18 @@ $(document).ready(function(){
     lat = lat.toString().replace(/\./, ",");
     lng = lng.toString().replace(/\./, ",");
           
-    if ($("input[value=upload]").attr("checked")) {
+    if (0 < $("#upload_fooQueue").children().length) {
       
-      $("body").append("<div id='upload_foo'></div>");
+      var gesture_path = "/locations/"+lat+";"+lng+";100,0/gestures";
       
       $.ajax({
         type: "POST",
-        url: "/locations/"+lat+";"+lng+";100,0/gestures",
+        url: gesture_path,
         data: "gesture[name]=" + $('input[name=gesture_name]:checked').val(),
         success: function(msg){
-          $("#upload_response").html(msg);
-          path = msg.split("/")[4].replace(/\"\}/, "");
-        
-          $('#upload_foo').uploadify({
-            'uploader':  '/uploadify.swf',
-            'cancelImg': '/images/cancel.png',
-            'fileDataName': 'upload[attachment]',
-            'auto':      'true',
-            'scriptData': {'_method' : 'put'}, 
-            'script':    "/uploads/" + path,
-            onAllComplete : function() {
-              $("#upload_foo").remove();
-            }
-          });
+          var upload_path = "/uploads/" + msg.split("/")[4].replace(/\"\}/, "");
+          $("#upload_foo").uploadifySettings('script', upload_path);
+          $("#upload_foo").uploadifyUpload(); 
         }
       });
     }
@@ -49,31 +39,16 @@ $(document).ready(function(){
     return false;
     
   });
-})
+});
 
-setup_overlay = function() {
-  map.clearOverlays();
-  
-  var zoom_level      = map.getZoom();
-  var max_zoom_level  = 17;
-  var zoom_diff       = max_zoom_level - zoom_level;
-  var scale_factor    = zoom_diff * 2;
-  
-  if (scale_factor == 0) {
-    scale_factor = 1;
+var uploader = {
+  initialize : function() {
+    $('#upload_foo').uploadify({
+      'uploader':  '/uploadify.swf',
+      'cancelImg': '/images/cancel.png',
+      'fileDataName': 'upload[attachment]',
+      'scriptData': {'_method' : 'put'}, 
+      'script':    "/uploads/"
+    });
   }
-  
-  var radius_xy       = 300;
-  
-  var zoomed_radius_xy  = (radius_xy / scale_factor);
-  var overlay_x         = (600-zoomed_radius_xy)/2;
-  var overlay_y         = (450-zoomed_radius_xy)/2;
-  
-  logo = new GScreenOverlay('/images/radius.png',
-          new GScreenPoint(overlay_x, overlay_y, 'pixels', 'pixels'),  // screenXY
-          new GScreenPoint(0, 0),  // overlayXY
-          new GScreenSize(zoomed_radius_xy, zoomed_radius_xy)  // size on screen
-        );
-  map.addOverlay(logo);
-  
 }

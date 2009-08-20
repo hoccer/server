16f8fb8b641f48ba8cdda4caab84cf81750b26ef
hukl <john@artcom.de>
2009-08-20 11:21:18 +0200

    re-enabling uploads

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,23 +1,10 @@
 class Upload < ActiveRecord::Base
-  belongs_to :peer
   
-  #  def attachment_ready?
-  #    !attachment.original_filename.nil?
-  #  end
-  #
-  #  def within_download_limits?
-  #    if self.download_limit.nil?
-  #      true
-  #    else
-  #      download_counter < self.download_limit
-  #    end
-  #  end
-  #  
-  #  def download_limit
-  #    if gesture.name == "pass"
-  #      1
-  #    else
-  #      nil
-  #    end
-  #  end
+  belongs_to :peer
+  has_attached_file :attachment
+  
+  def attachment_ready?
+    !attachment.original_filename.nil?
+  end
+
 end
--- test/functional/uploads_controller_test.rb
+++ test/functional/uploads_controller_test.rb
@@ -1,8 +1,55 @@
 require 'test_helper'
 
 class UploadsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "updating an upload" do
+    Upload.create :uid => "23"
+    
+    tmpfile = File.new(
+      File.join(RAILS_ROOT, "test", "fixtures", "upload_test.jpg")
+    )
+    
+    put( 
+      :update,
+      :upload => {:attachment => tmpfile},
+      :id => Upload.last.uid
+    )
+  end
+  
+  test "fetching an upload without an attachment" do
+    assert peer = Peer.create(
+      :latitude   => 13.44,
+      :longitude  => 52.12,
+      :accuracy   => 42.0,
+      :gesture    => "pass",
+      :seeder     => true
+    )
+    
+    assert peer.upload
+        
+    get :show, :id => peer.upload.uid
+    assert_response 204
+  end
+  
+  test "fetching an upload with an attachment" do
+    assert peer = Peer.create(
+      :latitude   => 13.44,
+      :longitude  => 52.12,
+      :accuracy   => 42.0,
+      :gesture    => "pass",
+      :seeder     => true
+    )
+
+    attachment = File.new(
+      File.join(RAILS_ROOT, "test", "fixtures", "upload_test.jpg")
+    )
+
+    put(
+      :update,
+      :upload => {:attachment => attachment},
+      :id => Upload.last.uid
+    )
+    
+    get :show, :id => peer.upload.uid
+    assert_response 200
   end
 end

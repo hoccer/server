ba1852c197433e17ea41b5dfbd99f25b203564d3
hukl <john@artcom.de>
2009-08-21 16:42:34 +0200

    added logging statement for analasys

--- app/controllers/peers_controller.rb
+++ app/controllers/peers_controller.rb
@@ -1,5 +1,6 @@
 class PeersController < ApplicationController
   skip_before_filter :verify_authenticity_token 
+  
   def create
     peer = Peer.create params[:peer]
     
@@ -14,10 +15,22 @@ class PeersController < ApplicationController
   end
 
   def show
-    peer    = Peer.find_by_uid(params[:id])
-    status  = peer.peer_group.status
+    peer        = Peer.find_by_uid(params[:id])
+    status      = peer.peer_group.status
+    log_peer_group_info(peer, status) unless status[:state] == :waiting
     status[:resources]  = status[:resources].map {|u| upload_url(:id => u)}
     render :json => status.to_json, :status => status[:status_code]
   end
 
+  private
+    def log_peer_group_info peer, status_hash
+      peer_group  = peer.peer_group
+      logger.info ">>>>>" \
+        "peer_group_id:#{peer_group.id} " \
+        "state:#{status_hash[:state]} " \
+        "peers:#{peer_group.number_of_peers} " \
+        "seeders:#{peer_group.number_of_seeders} " \
+        "gesture:#{peer_group.class.to_s} " \
+        "locations:#{peer_group.peers.map {|x| x.serialize_coordinates}.join("/")}"
+    end
 end

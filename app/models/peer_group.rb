cb9bf3bf1a47bb4f664f788a2284bdd9db36a7fa
hukl <john@artcom.de>
2009-10-19 12:49:27 +0200

    shortening message

--- app/models/peer_group.rb
+++ app/models/peer_group.rb
@@ -76,7 +76,7 @@ class PeerGroup < ActiveRecord::Base
     elsif expired? && 0 < number_of_seeders && 0 < number_of_peers
       {
         :state => :ready, 
-        :message => "Downloading the caught content",
+        :message => "Downloading content",
         :expires => 0, 
         :resources => peers.map {|p| p.upload.uid if p.upload}.compact,
         :status_code => 200

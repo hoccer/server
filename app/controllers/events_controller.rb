2b9fdecf569e2e3517c30fc532bfac2011d4fa0f
hukl <john@artcom.de>
2010-03-15 14:07:47 +0100

    added location_accuracy column to events table added events resource implemented create action functional tests for drop and pick

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,22 @@
 class EventsController < ApplicationController
+  
+  skip_before_filter :verify_authenticity_token
+  
   def index
   end
 
   def create
+    event_type  = params[:event].delete(:type)
+    base        = Event.const_get(event_type.titlecase)
+    
+    event = base.new params[:event]
+    
+    if event.save
+      redirect_to event_path(event), :status => 303
+    else
+      render :nothing => true, :status => 404
+    end
+    
   end
 
   def show
--- test/functional/events_controller_test.rb
+++ test/functional/events_controller_test.rb
@@ -1,8 +1,39 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "create drop event" do
+    
+    assert_difference ["Drop.count", "Deposit.count"], +1 do
+      post :create, :event => {
+        :type               => "drop",
+        :latitude           => 52.0,
+        :longitude          => 13.0,
+        :location_accuracy  => 100.0,
+        :starting_at        => Time.now,
+        :ending_at          => 7.seconds.from_now,
+        :access_point_sightings_attributes => [:bssid => "ffff", :bssid => "cccc"]
+      }
+    end
+    
+    assert_response       303 #redirect see other
+    assert_redirected_to  event_path(Event.last)
   end
+  
+  test "create pick event" do
+    assert_difference ["Pick.count"], +1 do
+      post :create, :event => {
+        :type               => "pick",
+        :latitude           => 52.0,
+        :longitude          => 13.0,
+        :location_accuracy  => 100.0,
+        :starting_at        => Time.now,
+        :ending_at          => (Time.now + 7.seconds),
+        :access_point_sightings_attributes => [:bssid => "ffff", :bssid => "cccc"]
+      }
+    end
+  end
+  
+  
+
 end

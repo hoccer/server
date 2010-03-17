f0584347600ede4208e5c913160f185786f6b8f6
hukl <john@artcom.de>
2010-03-17 14:30:43 +0100

    minor adaptions

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,7 +1,7 @@
 class Upload < ActiveRecord::Base
   
-  belongs_to :event
   belongs_to :peer
+  belongs_to :event
   
   has_attached_file( :attachment, :styles => { :processed => {} },
                      :processors => [ :vcard ])
--- vendor/plugins/old_server/config/routes.rb
+++ vendor/plugins/old_server/config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   
-  map.resources :uploads
   map.resources :peers
   
 end

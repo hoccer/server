4ba04ee95ebca4857f468ce2b070efe2c7005869
hukl <john@artcom.de>
2010-03-19 16:46:09 +0100

    removed old server engine as we now seem to fully wrap it in the new models

--- vendor/plugins/old_server/tasks/old_server_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :old_server do
-#   # Task goes here
-# end
--- vendor/plugins/old_server/test/old_server_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class OldServerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end

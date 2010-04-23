8fb897d27b4571002d2e453bce71084ee0dc6738
hukl <john@artcom.de>
2010-04-23 15:25:45 +0200

    updated info_hash method for sweeping

--- app/models/pass.rb
+++ app/models/pass.rb
@@ -21,41 +21,27 @@ module Pass
   end
   
   def info_hash
-    linked_events = nearby_events
-    
-    if collisions?
+    result = case current_state
+    when :collision
       {
         :state        => :collision,
         :message      => "Your hoc was intercepted. Try again.", # David said so
-        :uploads      => Event.extract_uploads(linked_events),
-        :peers        => linked_events.size,
+        :uploads      => [],
+        :expires      => 0,
+        :peers        => (event_group.events - [self]).size,
         :status_code  => 409
       }
-    elsif !expired?
-      info = {
+      
+    when :waiting
+      {
         :state        => :waiting,
         :message      => "waiting for other participants",
-        :expires      => ( ending_at - Time.now ),
-        :peers        => linked_events.size,
+        :expires      => expires,
+        :peers        => (event_group.events - [self]).size ,
         :status_code  => 202
       }
       
-      if self.class == SweepOut
-        info.merge(:upload_uri => upload.uuid)
-      else
-        info
-      end
-      
-    elsif expired? && 0 <  number_of_seeders && 0 == number_of_peers
-      {
-        :state        => :no_peers,
-        :message      => "Nobody caught your content.",
-        :uploads      => [],
-        :expires      => 0,
-        :peers        => linked_events.size,
-        :status_code  => 410
-      }
-    elsif expired? && 0 == number_of_seeders && 0 < number_of_peers
+    when :no_seeders
       {
         :state        => :no_seeders,
         :message      => "Nothing was thrown to you.",
@@ -64,15 +50,36 @@ module Pass
         :peers        => linked_events.size,
         :status_code  => 410
       }
-    elsif expired? && 0 < number_of_seeders && 0 < number_of_peers
+      
+    when :no_peers
+      {
+        :state        => :no_peers,
+        :message      => "Nobody caught your content.",
+        :uploads      => [],
+        :expires      => 0,
+        :peers        => (event_group.events - [self]).size,
+        :status_code  => 410
+      }
+      
+    when :ready
       {
         :state        => :ready,
         :message      => "content available for download",
-        :uploads      => Event.extract_uploads(nearby_events(:types => seeder)),
-        :peers        => linked_events.size,
+        :uploads      => Event.extract_uploads(event_group.events),
+        :peers        => (event_group.events - [self]).size,
         :status_code  => 200
       }
+      
     end
+    
+    if upload
+      result.merge({
+        :upload_uri => upload.uuid
+      })
+    else
+      result
+    end
+    
   end
   
 end

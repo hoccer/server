9f480158d4b52076bf1675cf868abadea36dd2a6
John <john-paul.bader@artcom.de>
2010-08-27 16:37:52 +0200

    spaces

--- app/models/hoccer/distribute.rb
+++ app/models/hoccer/distribute.rb
@@ -1,26 +1,26 @@
 module Hoccer
   module Distribute
-    
+
     def seeder
       "Throw"
     end
-    
+
     def peer
       "Catch"
     end
-    
+
     def collisions?
       1 < number_of_seeders
     end
-  
+
     def number_of_peers
       event_group.events.with_type( peer ).count
     end
-  
+
     def number_of_seeders
       event_group.events.with_type( seeder ).count
     end
-    
+
     def expiration_time
       reference = Event.first(
         :select => "ending_at, created_at, event_group_id",
@@ -30,10 +30,10 @@ module Hoccer
 
       reference ? reference.ending_at : self.ending_at
     end
-    
+
     def info_hash
       result = case current_state
-        
+
       when :waiting
         {
           :state        => "waiting",
@@ -42,7 +42,7 @@ module Hoccer
           :peers        => (event_group.events - [self]).size,
           :status_code  => 202
         }
-        
+
       when :collision
         {
           :state        => "collision",
@@ -51,7 +51,7 @@ module Hoccer
           :peers        => (event_group.events - [self]).size,
           :status_code  => 409
         }
-        
+
       when :no_peers
         {
           :state        => "no_peers",
@@ -60,7 +60,7 @@ module Hoccer
           :peers        => (event_group.events - [self]).size,
           :status_code  => 410
         }
-        
+
       when :no_seeders
         {
           :state        => "no_seeders",
@@ -69,7 +69,7 @@ module Hoccer
           :peers        => (event_group.events - [self]).size,
           :status_code  => 410
         }
-        
+
       when :ready
         {
           :state        => "ready",
@@ -79,7 +79,7 @@ module Hoccer
           :uploads      => Event.extract_uploads(event_group.events),
           :status_code  => 200
         }
-        
+
       when :canceled
         {
           :state        => :canceled,
@@ -99,9 +99,9 @@ module Hoccer
           :peers        => 0,
           :status_code  => 400
         }
-        
+
       end
-      
+
       if upload
         result.merge({
           :upload_uri => upload.uuid

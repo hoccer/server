8cd6369835942781bedd48839a1228e15f2479c2
Rodja Trappe <rodja@hoccer.com>
2010-09-01 13:46:16 +0200

    testing delayed send and receive

--- experiments/server_test.rb
+++ experiments/server_test.rb
@@ -8,7 +8,7 @@ require "client"
 
 class ServerTest < Test::Unit::TestCase
 
-  def test_simple_pairing
+  def test_pairing_immediately
     sc = Client.new 33.324, 22.112, 100
     rc = Client.new 33.321, 22.115, 100
 
@@ -17,6 +17,27 @@ class ServerTest < Test::Unit::TestCase
     rt = Thread.new{rc.receive :pass}
     assert_equal data, rt.value
   end
+
+  def test_pairing_with_delayed_receive
+    sc = Client.new 33.324, 22.112, 100
+    rc = Client.new 33.321, 22.115, 100
+
+    data = "{...}"
+    st = Thread.new{sc.send :pass, data}
+    rt = Thread.new{sleep 1; rc.receive :pass}
+    assert_equal data, rt.value
+  end
+
+  def test_pairing_with_delayed_send
+    sc = Client.new 33.324, 22.112, 100
+    rc = Client.new 33.321, 22.115, 100
+
+    data = "{...}"
+    st = Thread.new{sleep 1; sc.send :pass, data}
+    rt = Thread.new{rc.receive :pass}
+    assert_equal data, rt.value
+  end
+
 
   def test_snappiness
     sc = Client.new 33.324, 22.112, 100
@@ -35,7 +56,9 @@ class ServerTest < Test::Unit::TestCase
     }
     st.join
     rt.join
-    puts "snappiness is #{in_time - out_time}"
+    snappiness = in_time - out_time
+    puts "snappiness: #{snappiness}"
+    assert_in_delta 0.1, snappiness, 0.01
   end
 
 end

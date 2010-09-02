276f1e1e91ad7f0165a496467b7f2836206c3ac6
Rodja Trappe <rodja@hoccer.com>
2010-09-02 09:30:09 +0200

    parsing json from initial POST

--- experiments/client.rb
+++ experiments/client.rb
@@ -1,17 +1,13 @@
-require "rubygems"
-require "active_support"
 require 'digest/sha1'
 require 'net/http'
+require 'json'
 
 class Client
 
   def initialize lat=nil, long=nil, accuracy=nil
-    #@simulate = true;
 
-    @server = "localhost"
-    id = http_post "#/v3/clients", "{client:'ruby tester'}"
-    id = Digest::SHA1.hexdigest Time.now.to_s
-    @client_uri = "/v3/clients/#{id}"
+    response = http_post "http://beta.hoccer.com/v3/clients", "{client:'ruby tester'}"
+    @uri = JSON.parse(response)[:uri]
 
     set_gps lat, long, accuracy if lat
   end
@@ -19,57 +15,45 @@ class Client
   def set_gps lat, long, accuracy
     environment = {:gps => {:latitude => lat, :longitude => long, :accuracy => accuracy}}
 
-    http_put "#{@client_uri}/environment\n#{environment.to_json}"
+    http_put "#{@uri}/environment", "{environment.to_json}"
   end
 
   def send mode, payload
-    http_post "#{@client_uri}/action/#{mode}", "#{payload}"
+    http_post "#{@uri}/action/#{mode}", "#{payload}"
     raise NoOneReceivedError
   end
 
   def receive mode
-    http_get "#{@client_uri}/action/#{mode}"
+    http_get "#{@uri}/action/#{mode}"
   end
 
   private
 
   def http_get uri
-    unless @simulate
-      Net::HTTP.start(@server) {|http|
-        response = http.get uri
-        raise NoOneSharedError if response.code == 410
-        raise CollisionError if response.code == 409
-        return response.body
-      }
-    else
-      puts "GET #{uri}"
-      "no data in simulation mode"
-    end
-
+    uri = URI.parse(uri)
+    Net::HTTP.start(uri.host) {|http|
+      response = http.get uri.path
+      raise NoOneSharedError if response.code == 410
+      raise CollisionError if response.code == 409
+      return response.body
+    }
   end
 
   def http_put uri, payload
-    unless @simulate
-      Net::HTTP.start(@server) {|http|
-        return (http.request_put uri, payload).body
-      }
-    else
-      puts "PUT #{uri}\n#{payload}"
-    end
+    uri = URI.parse(uri)
+    Net::HTTP.start(uri.host) {|http|
+      return (http.request_put uri.path, payload).body
+    }
   end
 
   def http_post uri, payload
-    unless @simulate
-      Net::HTTP.start(@server) {|http|
-        response = http.request_post uri, payload
-        raise NoOneReceivedError if response.code == 410
-        raise CollisionError if response.code == 409
-        return response.body
-      }
-    else
-      puts "POST #{uri}\n#{payload}"
-      "no data in simulation mode"
-    end
+    uri = URI.parse(uri)
+    Net::HTTP.start(uri.host) {|http|
+      response = http.request_post uri.path, payload
+      raise NoOneReceivedError if response.code == 410
+      raise CollisionError if response.code == 409
+      return response.body
+    }
   end
 
 end

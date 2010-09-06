90ef802f73e62a01eaee43a66a70f151d7cd5b2a
John <john-paul.bader@artcom.de>
2010-09-06 18:16:03 +0200

    more implementation 

--- v3/hoccer.rb
+++ v3/hoccer.rb
@@ -1,8 +1,11 @@
+require 'uuid'
+require 'ruby-debug'
+
 module Hoccer
 
   class Client
 
-    attr_accessor :client_id, :request
+    attr_accessor :client_id, :request, :environment
 
     def initialize client_id, request
       @client_id = client_id
@@ -29,6 +32,11 @@ module Hoccer
       @pool
     end
 
+    def add_user
+      uuid = UUID.generate(:compact)
+      @pool[uuid] = Client.new uuid, nil
+    end
+
   end
 
   class App < Sinatra::Base
@@ -37,17 +45,45 @@ module Hoccer
 
     @@client_pool = ClientPool.new
 
-    def client_id
-      rand(2**16)
+    def pool
+      @@client_pool.clients
     end
 
-    aget "/hi" do
-      @client = Client.new client_id, self
+    def client_pool
+      @@client_pool
+    end
+
+    aget "/high" do
+      body { "hello" }
+
+    end
+
+    post "/clients" do
+      client = client_pool.add_user
+      redirect "/clients/#{client.client_id}", 303
+    end
+
+    get %r{/clients/([a-f0-9]{32,32}$)} do |uuid|
+      if pool[uuid]
+        body { "yay" }
+      else
+        halt 412
+      end
+    end
+
+    aget "/xxxx" do
+      #client = @@client_pool.clients[uuid]
+      #client.environment = params.keys.first
+      body { "hello" }
+    end
+
+    aget "/clients/:uuid/actions/:action" do |uuid, action|
+      client = Client.new uuid, self
       @@client_pool.insert @client
 
       EM::Timer.new(7) do
-        @@client_pool.clients[@client.client_id].request.body { "hello" }
-        @@client_pool.remove @client
+        @@client_pool.clients[client.client_id].request.body { "hello" }
+        @@client_pool.remove client
       end
 
       timer = EventMachine::PeriodicTimer.new(0.1) do
--- v3/test/unit/hoccer_test.rb
+++ v3/test/unit/hoccer_test.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", ".."))
 require 'test/unit'
-require 'rack/test'
+require 'eventmachine'
 require 'sinatra/async'
 require "sinatra/async/test"
 require 'hoccer'
@@ -9,17 +9,49 @@ class TestHoccer < Test::Unit::TestCase
   include Sinatra::Async::Test::Methods
 
   def app
-    Hoccer::App.new
+    @app ||=Hoccer::App.new
+  end
+
+  def app= sin_app
+    @app = sin_app
   end
 
   def test_my_tests
     assert true
   end
 
-  def test_my_app
-    get "/client/uuid/action/id"
+  def test_basic_route
+    get "high"
     assert_async
-    async_continue
-    assert_equal 200, last_response.status
   end
+
+  def test_creating_new_uuid
+    post "clients"
+    assert_equal 303, last_response.status
+    assert last_response.headers["Location"] =~ /\/clients\/[a-z0-9]/
+  end
+
+  def test_client_self_reference
+    post "clients"
+    my_uuid = last_response.headers["Location"].match(/[a-z0-9]+$/)[0]
+
+    get "/clients/#{my_uuid}"
+    assert_equal "yay", last_response.body
+  end
+
+  def test_self_reference_with_malicious_uuid
+    get "/clients/c7dde7f09bef012d8e37001f5bf1b5ba"
+    assert_equal 412, last_response.status
+  end
+
+  def test_putting_the_environment
+    #post "clients"
+    #my_uuid = last_response.headers["Location"].match(/[a-z0-9]+$/)[0]
+    l_app = app
+    get "/high"
+    assert_async
+    app = l_app
+    async_continue
+  end
+
 end

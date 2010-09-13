4bd63ee480c3ffcc9e7189205eb0dfdab15a6013
John <john-paul.bader@artcom.de>
2010-09-13 18:33:51 +0200

    added integration tests and TestClient 

--- v3/test/integration/requests_test.rb
+++ v3/test/integration/requests_test.rb
@@ -1,67 +1,60 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), ".."))
+require 'helper'
+require 'test_client'
 require 'net/http'
-require 'json'
 
+class TestRequest < Test::Unit::TestCase
 
-server = "127.0.0.1"
-port   = "9292"
+  test "registering clients" do
+    client_1 = TestClient.new
+    client_1.register
 
-client_1_url = ""
+    client_2 = TestClient.new
+    client_2.register
 
-Net::HTTP.start(server, port) {|http|
-  response = http.request_post "/clients", ""
-  client_1_url = response.header["Location"]
-}
+    assert_not_nil client_1.uuid
+    assert_not_nil client_2.uuid
+    assert client_1.uuid != client_2.uuid
+    client_1.delete_environment
+    client_2.delete_environment
+  end
 
-client_2_url = ""
+  test "updating the environment" do
+    client = TestClient.create
+    assert_not_nil client.uuid
 
-Net::HTTP.start(server, port) {|http|
-  response = http.request_post "/clients", ""
-  client_2_url = response.header["Location"]
-}
+    response = client.update_environment({
+      :gps => { :latitude => 32.22, :longitude => 88.74 }
+    })
 
-payload = {:gps => {:longitude => 11, :latitude => 12}}.to_json
+    assert_equal "200", response.header.code
+    client.delete_environment
+  end
 
-Net::HTTP.start(server, port) {|http|
-  response = http.request_put "#{client_1_url}/environment", payload
-  puts response
-}
-Net::HTTP.start(server, port) {|http|
-  response = http.request_put "#{client_2_url}/environment", payload
-  puts response
-}
+  test "lonesome client tries to share" do
+    client = TestClient.create
+    client.update_environment({
+      :gps => { :latitude => 12.22, :longitude => 18.74 }
+    })
+    client.share( "pass", {:inline => "hello"} )
+    assert_equal "204", client.follow_redirect.header.code
 
+    client.delete_environment
+  end
 
+  test "lonesome client tries to receive" do
+    client = TestClient.create
+    client.update_environment({
+      :gps => { :latitude => 52.22, :longitude => 28.74 }
+    })
 
-shareload = {:inline => "hallo robert"}.to_json
-client_1_action_url = ""
-Net::HTTP.start(server, port) {|http|
-  response = http.request_post "#{client_1_url}/action/distribute", shareload
-  client_1_action_url = response.header["Location"]
-}
-t1_response = ""
-t1 = Thread.new do
-  Net::HTTP.start(server, port) {|http|
-    t1_response = http.get( "#{client_1_url}/action/distribute" )
-    puts "OhHAi " + response.body
-  }
+    assert_equal "204", client.receive( "pass" ).header.code
+
+    client.delete_environment
+  end
+
+  test "two clients only one action" do
+
+  end
+
 end
-
-
-t2 = Thread.new do
-  Net::HTTP.start(server, port) {|http|
-    t2_response = http.get( "#{client_2_url}/action/distribute" )
-    puts "OhHAi " + t2_response.body
-  }
-end
-
-sleep(0.2)
-puts "hello"
-
-
-
-__END__
-Net::HTTP.start(server, port) {|http|
-  response = http.post ""
-  puts response.body
-}
-

7e8b9be9f87a006b5bb2b8cfb1f6b2f9d3819032
John <john-paul.bader@artcom.de>
2010-09-21 13:11:01 +0200

    more testclient implementation

--- v3/storage/test/test_client.rb
+++ v3/storage/test/test_client.rb
@@ -12,21 +12,72 @@ class TestClient
     @port   = 9292
   end
 
-  def drop data
-    post "/store", data.to_json
+  def validate options, key, class_name
+    unless options[key] && options[key].is_a?( class_name )
+      raise(
+        ArgumentError,
+        "#{key.capitalize} is missing or not a #{class_name.to_s}"
+      )
+    end
+  end
+
+  def prepare options
+    validate options, :longitude, Float
+    validate options, :latitude,  Float
+    validate options, :params,    Hash
+
+    {
+      :environment => {
+        :gps => {
+          :longitude  => options[:longitude],
+          :latitude   => options[:latitude],
+          :accuracy   => options[:accuracy] || 5
+        }
+      },
+      :params   => options[:params],
+      :lifetime => options[:liftime]
+    }
+  end
+
+  def store options
+    data = prepare( options )
+    post "/store", data
   end
 
   def remove uuid
     delete "/store/#{uuid}"
   end
 
-  def query data
-    post "/query", data.to_json
+  def search_within_radius options
+    validate options, :longitude, Float
+    validate options, :latitude,  Float
+    validate options, :radius,    Float
+
+    options[:accuracy] = options.delete(:radius)
+
+    post "/query", { :gps => options }
   end
 
-  def request method, path, data = ""
-    Net::HTTP.start(@server, @port) do |http|
-      http.send( "request_#{method}".to_sym, path, data )
+  def search_within_region options
+    validate options, :region,    Array
+
+    unless options[:region].all? {|x| x.is_a? Array}
+      raise ArgumentError, "Region must be of format [[lon1,lat1], [lon2,lat2]]"
+    end
+
+    post "/query", { :box => options[:region] }
+  end
+
+  def request method, path, data = {}
+    if method == :delete
+      Net::HTTP.start(@server, @port) do |http|
+        req = Net::HTTP::Delete.new(path)
+        response = http.request(req)
+      end
+    else
+      Net::HTTP.start(@server, @port) do |http|
+        http.send( "request_#{method}".to_sym, path, data.to_json)
+      end
     end
   end
 
@@ -39,3 +90,5 @@ class TestClient
   end
 
 end
+
+

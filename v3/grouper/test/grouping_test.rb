4ce052f8e69db5a9177fe80648fafa9a121636f6
John <john-paul.bader@artcom.de>
2010-10-22 12:30:03 +0200

    spaces

--- v3/grouper/test/grouping_test.rb
+++ v3/grouper/test/grouping_test.rb
@@ -6,7 +6,7 @@ class ExhibitTest < Test::Unit::TestCase
 
   def new_location
     @gps ||= [13, 52]
-    @gps.map! { |element| element += 1 } 
+    @gps.map! { |element| element += 1 }
 
     operand = (rand(2) % 2) == 0 ? +1 : -1
     {
@@ -18,70 +18,70 @@ class ExhibitTest < Test::Unit::TestCase
       }
     }
   end
-  
-  def teardown 
+
+  def teardown
     Environment.delete_all
   end
-  
+
   test 'grouping two clients' do
     location = new_location
     location.merge!(:client_uuid => 1)
 
     a  = Environment.create(location)
     assert a[:group_id], "should have a group id"
-    
+
     location.merge!(:client_uuid => 2)
     b = Environment.create(location)
 
     a.reload
 
-    assert_equal b.group.count, 2, "should have grouped environments"   
-    assert_equal a.group.count, 2, "should have grouped environments"   
-    
+    assert_equal b.group.count, 2, "should have grouped environments"
+    assert_equal a.group.count, 2, "should have grouped environments"
+
     assert_equal a[:group_id], b[:group_id], "group ids should match"
     assert_equal b.group, a.group
-    
+
     assert JSON.parse(a.group.to_json)
-  end  
-  
+  end
+
   test 'not grouping clients' do
     location = new_location
     location.merge!(:client_uuid => 1)
     a  = Environment.create(location)
-    
+
     location2 = new_location
     location2.merge!(:client_uuid => 2)
     b = Environment.create(location2)
-    
-    assert_not_equal a["group_id"], b["group_id"]    
-  end  
-  
+
+    assert_not_equal a["group_id"], b["group_id"]
+  end
+
   test 'lonley group' do
     location = new_location
     location.merge!(:client_uuid => 1)
     a  = Environment.create(location)
-    
+
     assert_equal a.group.count, 1, "should have at least self"
   end
-  
+
   test 'get newest client update' do
      location = new_location
      location.merge!(:client_uuid => 1)
      Environment.create(location)
-     
+
      location = new_location
      location.merge!(:client_uuid => 1)
      Environment.create(location)
-      
+
      location = new_location
      location.merge!(:client_uuid => 1)
-     newest_added = Environment.create(location) 
-     
+     newest_added = Environment.create(location)
+
      newest_find = Environment.newest 1
-     
+
      assert_equal newest_added, newest_find, "should find last added environment"
   end
-  
-  
-  
+
+
+
 end

afa192e3c2602560711f150de9edd6419a2bef7e
Robert Palmer <hi@robertpalmer.de>
2010-11-02 00:12:05 +0100

    added one-to-many class, all tests pass for now

--- v3/linker/lib/hoccer.rb
+++ v3/linker/lib/hoccer.rb
@@ -23,7 +23,7 @@ end
  
 module Hoccer
   
-  class OneToOne 
+  class Event
     def initialize action_store
       @action_store = action_store
     end
@@ -46,18 +46,27 @@ module Hoccer
             verify group, true
             @action_store.invalidate uuid
           end
-        end
-        
+        end        
       end
     end
-       
+    
+    def parse_group json_string
+      begin
+        group = JSON.parse json_string
+      rescue => e
+        puts e
+        group = {}
+      end
+      group
+    end
+    
+  end
+  
+  class OneToOne < Event
     def verify group, reevaluate = false
       actions = @action_store.actions_in_group(group, "one-to-one")
       sender   = actions.select { |c| c[:type] == :sender }
       receiver = actions.select { |c| c[:type] == :receiver }
-      
-      puts "sender   #{sender.count}"
-      puts "receiver #{receiver.count}"
       
       if sender.size > 1 || receiver.size > 1
         actions.each do |client|
@@ -72,17 +81,31 @@ module Hoccer
         end
       end
     end
-
-    def parse_group json_string
-      begin
-        group = JSON.parse json_string
-      rescue => e
-        puts e
-        group = {}
-      end
-      group
-    end
   end
+  
+  class OneToMany < Event
+    def verify group, reevaluate = false
+      actions = @action_store.actions_in_group(group, "one-to-many")
+      sender   = actions.select { |c| c[:type] == :sender }
+      receiver = actions.select { |c| c[:type] == :receiver }
+      
+      if sender.size > 1
+        actions.each do |client|
+          @action_store.conflict client[:uuid]
+        end
+      elsif sender.size == 1 && receiver.size >= 1
+        data_list = sender.map { |s| s[:payload] }
+        Logger.successful_actions actions        
+        
+        actions.each do |client|
+          @action_store.send client[:uuid], data_list
+        end
+      end
+    end
+    
+    
+  end
+  
   
   
   class App < Sinatra::Base
@@ -98,6 +121,7 @@ module Hoccer
     def initialize 
        super
        @@evaluators['one-to-one'] = OneToOne.new @@action_store
+       @@evaluators['one-to-many'] = OneToMany.new @@action_store
     end
          
      aget %r{#{CLIENTS}$} do |uuid|
@@ -144,7 +168,7 @@ module Hoccer
     aget %r{#{CLIENTS}/action/([\w-]+)} do |uuid, action_name|
       action = { :mode => action_name, :type => :receiver, :request => self, :uuid => uuid }
       
-      @@evaluators['one-to-one'].add action
+      @@evaluators[action_name].add action
     end 
   end
 

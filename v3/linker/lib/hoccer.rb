6d9b005e557f43fab49febae0d6d997e6b66a4de
Robert Palmer <hi@robertpalmer.de>
2010-11-01 23:42:45 +0100

    moved onetoone in own class

--- v3/linker/lib/hoccer.rb
+++ v3/linker/lib/hoccer.rb
@@ -3,34 +3,47 @@ require 'logger'
 require 'action_store'
 
 CLIENTS = "/clients/([a-zA-Z0-9\-]{36,36})"
-                           
+  
+SERVER        = "localhost"
+PORT          = 4567
+
+def em_request path, method, content, &block
+  http = EM::Protocols::HttpClient.request(
+    :host => SERVER,
+    :port => PORT,
+    :verb => method   ||= "GET",
+    :request => path  ||= "/",
+    :content => content
+  )
+
+  http.callback do |response|
+    block.call( response )
+  end
+end
+ 
 module Hoccer
   
-  class App < Sinatra::Base
-    register Sinatra::Async
-    
-    configure(:development) do
-      register Sinatra::Reloader
+  class OneToOne 
+    def initialize action_store
+      @action_store = action_store
     end
-    
-    @@server        = "localhost"
-    @@port          = 4567
-    @@action_store  = ActionStore.new
-    
-    def em_request path, method, content, &block
-      http = EM::Protocols::HttpClient.request(
-        :host => @@server,
-        :port => @@port,
-        :verb => method   ||= "GET",
-        :request => path  ||= "/",
-        :content => content
-      )
 
-      http.callback do |response|
-        block.call( response )
+    def add action
+      @action_store.hold_action_for_seconds action, 2
+      uuid = action[:uuid]
+
+      em_request( "/clients/#{uuid}/group", nil, nil ) do |response|
+        group = parse_group response[:content] 
+        
+        if group.size < 2
+          @action_store.invalidate action[:uuid]
+        else          
+          verify_one_to_one @action_store.actions_in_group(group, "one-to-one"), group  
+        end
       end
+      
     end
-
+       
     def verify_one_to_one clients, group, reevaluate = false
       sender   = clients.select { |c| c[:type] == :sender }
       receiver = clients.select { |c| c[:type] == :receiver }
@@ -40,7 +53,7 @@ module Hoccer
       
       if sender.size > 1 || receiver.size > 1
         clients.each do |client|
-          @@action_store.conflict client[:uuid]
+          @action_store.conflict client[:uuid]
         end
         return
       end
@@ -50,7 +63,7 @@ module Hoccer
         Logger.successful_actions clients        
         
         clients.each do |client|
-          @@action_store.send client[:uuid], data_list
+          @action_store.send client[:uuid], data_list
         end
         return
       end
@@ -63,7 +76,37 @@ module Hoccer
       
     end
 
-    aget %r{#{CLIENTS}$} do |uuid|
+    def parse_group json_string
+      begin
+        group = JSON.parse json_string
+      rescue => e
+        puts e
+        group = {}
+      end
+      
+      group
+    end
+    
+
+  end
+  
+  
+  class App < Sinatra::Base
+    register Sinatra::Async
+    
+    configure(:development) do
+      register Sinatra::Reloader
+    end
+    
+    @@action_store  = ActionStore.new
+    @@evaluators    = {}
+     
+    def initialize 
+       super
+       @@evaluators['one-to-one'] = OneToOne.new @@action_store
+    end
+         
+     aget %r{#{CLIENTS}$} do |uuid|
       em_request( "/clients/#{uuid}", nil, nil ) do |response|
         if response[:status] == 200
           body { response[:content] }
@@ -101,42 +144,17 @@ module Hoccer
         :uuid    => uuid
       }
       
-      @@action_store.hold_action_for_seconds action, 2
-      
-      em_request( "/clients/#{uuid}/group", nil, request.body.read ) do |response|
-        group = parse_group response[:content] 
-        
-        if group.size < 2
-          @@action_store.invalidate uuid
-        else          
-          verify_one_to_one @@action_store.actions_in_group(group, action_name), group  
-        end
-      end
+      @@evaluators['one-to-one'].add action
     end
 
     aget %r{#{CLIENTS}/action/([\w-]+)} do |uuid, action_name|
       action = { :mode => action_name, :type => :receiver, :request => self, :uuid => uuid }
       
-      @@action_store.hold_action_for_seconds action, 2
-
-      em_request( "/clients/#{uuid}/group", nil, request.body.read ) do |response|
-        group = parse_group response[:content] 
-        verify_one_to_one @@action_store.actions_in_group(group, action_name), group  
-      end
+      #body "okay"
+      @@evaluators['one-to-one'].add action
     end 
     
     private
-    
-    def parse_group json_string
-      begin
-        group = JSON.parse json_string
-      rescue => e
-        puts e
-        group = {}
-      end
-      
-      group
-    end
   end
 
 end

50242b7902fd67d25f4004e9531ffef94994985a
Robert Palmer <hi@robertpalmer.de>
2010-11-01 23:50:19 +0100

    cleanup

--- v3/linker/lib/hoccer.rb
+++ v3/linker/lib/hoccer.rb
@@ -29,51 +29,48 @@ module Hoccer
     end
 
     def add action
-      @action_store.hold_action_for_seconds action, 2
       uuid = action[:uuid]
-
+      @action_store[uuid] = action
+      
       em_request( "/clients/#{uuid}/group", nil, nil ) do |response|
         group = parse_group response[:content] 
         
         if group.size < 2
           @action_store.invalidate action[:uuid]
         else          
-          verify_one_to_one @action_store.actions_in_group(group, "one-to-one"), group  
+          verify group  
         end
+        
+        EM::Timer.new(2) do
+          if @action_store[uuid]
+            verify group, true
+            @action_store.invalidate uuid
+          end
+        end
+        
       end
-      
     end
        
-    def verify_one_to_one clients, group, reevaluate = false
-      sender   = clients.select { |c| c[:type] == :sender }
-      receiver = clients.select { |c| c[:type] == :receiver }
+    def verify group, reevaluate = false
+      actions = @action_store.actions_in_group(group, "one-to-one")
+      sender   = actions.select { |c| c[:type] == :sender }
+      receiver = actions.select { |c| c[:type] == :receiver }
       
       puts "sender   #{sender.count}"
       puts "receiver #{receiver.count}"
       
       if sender.size > 1 || receiver.size > 1
-        clients.each do |client|
+        actions.each do |client|
           @action_store.conflict client[:uuid]
         end
-        return
-      end
-      
-      if sender.size == 1 && receiver.size == 1 && (group.size == 2 || reevaluate)
+      elsif sender.size == 1 && receiver.size == 1 && (group.size == 2 || reevaluate)
         data_list = sender.map { |s| s[:payload] }
-        Logger.successful_actions clients        
+        Logger.successful_actions actions        
         
-        clients.each do |client|
+        actions.each do |client|
           @action_store.send client[:uuid], data_list
         end
-        return
       end
-      
-      unless reevaluate 
-        EM::Timer.new(1) do
-          verify_one_to_one clients, group, true
-        end
-      end
-      
     end
 
     def parse_group json_string
@@ -83,11 +80,8 @@ module Hoccer
         puts e
         group = {}
       end
-      
       group
     end
-    
-
   end
   
   
@@ -136,7 +130,7 @@ module Hoccer
     aput %r{#{CLIENTS}/action/([\w-]+)} do |uuid, action_name|
       payload       = JSON.parse( request.body.read )
 
-      action= {
+      action = { 
         :mode     => action_name,
         :type     => :sender,
         :payload  => payload,
@@ -150,11 +144,8 @@ module Hoccer
     aget %r{#{CLIENTS}/action/([\w-]+)} do |uuid, action_name|
       action = { :mode => action_name, :type => :receiver, :request => self, :uuid => uuid }
       
-      #body "okay"
       @@evaluators['one-to-one'].add action
     end 
-    
-    private
   end
 
 end

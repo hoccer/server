0dbc901bab7a61faf9381af262b498614f68341d
John <john-paul.bader@artcom.de>
2010-11-08 12:06:13 +0100

    require api keys for linker only in production environment

--- v3/linker/lib/helper.rb
+++ v3/linker/lib/helper.rb
@@ -33,26 +33,31 @@ def method_missing symbol, *args, &block
 end
 
 def authorized_request &block
-  EM.next_tick do
-    db          = EM::Mongo::Connection.new.db('hoccer_development')
-    collection  = db.collection('accounts')
 
-    collection.first("api_key" => params["apiKey"]) do |account|
-      if account.nil?
-        ahalt 401
-      else
-        signature = params.delete("signature")
-        uri       = env['REQUEST_URI'].gsub(/\&signature\=.+$/, "")
+  if ENV["RACK_ENV"] == "production"
+    EM.next_tick do
+      db          = EM::Mongo::Connection.new.db('hoccer_development')
+      collection  = db.collection('accounts')
 
-        digestor = Digest::HMAC.new(account["shared_secret"], Digest::SHA1)
-        computed_signature = digestor.base64digest(uri)
-
-        if signature == computed_signature
-          block.call( account )
-        else
+      collection.first("api_key" => params["apiKey"]) do |account|
+        if account.nil?
           ahalt 401
+        else
+          signature = params.delete("signature")
+          uri       = env['REQUEST_URI'].gsub(/\&signature\=.+$/, "")
+
+          digestor = Digest::HMAC.new(account["shared_secret"], Digest::SHA1)
+          computed_signature = digestor.base64digest(uri)
+
+          if signature == computed_signature
+            block.call( account )
+          else
+            ahalt 401
+          end
         end
       end
     end
+  else
+    block.call
   end
 end

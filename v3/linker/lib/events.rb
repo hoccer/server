9c67f5c21f129303337334a1619686732a5fc9a5
Robert Palmer <hi@robertpalmer.de>
2011-01-12 16:17:45 +0100

    extracted conflict? and success? methods, removed code dublication

--- v3/linker/lib/events.rb
+++ v3/linker/lib/events.rb
@@ -1,6 +1,7 @@
 require 'helper'
 
 module Hoccer
+  
   class Event
 
     def initialize action_store
@@ -28,7 +29,26 @@ module Hoccer
         end
       end
     end
+    
+    def verify group, reevaluate = false
+      actions = @action_store.actions_in_group(group, name)
+      sender   = actions.select { |c| c[:type] == :sender }
+      receiver = actions.select { |c| c[:type] == :receiver }
+      
+      puts "verifying group (#{group.size}) with #{actions.size} actions with #{sender.size} senders and #{receiver.size} receivers"
 
+      if conflict? sender, receiver
+        conflict actions
+      elsif success? sender, receiver, group, reevaluate
+        data_list = sender.map { |s| s[:payload] }
+        Logger.successful_actions actions
+
+        actions.each do |client|
+          @action_store.send client[:uuid], data_list
+        end
+      end
+    end
+    
     def conflict actions
       actions.each do |client|
         @action_store.conflict client[:uuid]
@@ -54,56 +74,40 @@ module Hoccer
 
   class OneToOne < Event
 
+    def name
+      "one-to-one"
+    end
+
     def timeout
       2
     end
 
-    def verify group, reevaluate = false
-      actions = @action_store.actions_in_group(group, "one-to-one")
-      sender   = actions.select { |c| c[:type] == :sender }
-      receiver = actions.select { |c| c[:type] == :receiver }
-      
-      puts "verifying group (#{group.size}) with #{actions.size} actions with #{sender.size} senders and #{receiver.size} receivers"
-
-      if sender.size > 1 || receiver.size > 1
-        conflict actions
-      elsif sender.size == 1 && receiver.size == 1 && (group.size == 2 || reevaluate)
-        data_list = sender.map { |s| s[:payload] }
-        Logger.successful_actions actions
-
-        actions.each do |client|
-          @action_store.send client[:uuid], data_list
-        end
-      end
+    def conflict? sender, receiver
+      sender.size > 1 || receiver.size > 1
+    end
+    
+    def success? sender, receiver, group, reevaluate
+      sender.size == 1 && receiver.size == 1 && (group.size == 2 || reevaluate)
     end
   end
 
   class OneToMany < Event
+    def name
+      "one-to-many"
+    end
+    
     def timeout
       7
     end
 
-    def verify group, reevaluate = false
-      actions = @action_store.actions_in_group(group, "one-to-many")
-      sender   = actions.select { |c| c[:type] == :sender }
-      receiver = actions.select { |c| c[:type] == :receiver }
-
-      puts "verifying group (#{group.size}) with #{actions.size} actions with #{sender.size} senders and #{receiver.size} receivers"
-
-      if sender.size > 1
-        actions.each do |client|
-          @action_store.conflict client[:uuid]
-        end
-      elsif sender.size == 1 && receiver.size >= 1 && (sender.size + receiver.size == group.size || reevaluate)
-
-        data_list = sender.map { |s| s[:payload] }
-        Logger.successful_actions actions
-
-        actions.each do |client|
-          @action_store.send client[:uuid], data_list
-        end
-      end
+    def conflict? sender, receiver
+      sender.size > 1
     end
+    
+    def success? sender, receiver, group, reevaluate
+      sender.size == 1 && receiver.size >= 1 && (sender.size + receiver.size == group.size || reevaluate)
+    end
+    
   end
 end
 

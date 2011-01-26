bc0f3a2def99f081e8062632f23d886d1643368b
Robert Palmer <hi@robertpalmer.de>
2011-01-26 10:52:53 +0100

    class instead of member variable for db connection

--- v3/linker/lib/helper.rb
+++ v3/linker/lib/helper.rb
@@ -59,8 +59,8 @@ def authorized_request &block
   
   if ENV["RACK_ENV"] == "production"
     EM.next_tick do
-      @db         ||= EM::Mongo::Connection.new.db( Hoccer.config["database"] )
-      collection  = @db.collection('accounts')
+      @@db         ||= EM::Mongo::Connection.new.db( Hoccer.config["database"] )
+      collection  = @@db.collection('accounts')
 
       collection.first("api_key" => params["api_key"]) do |account|
         if account.nil?
